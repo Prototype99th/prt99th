86cc3a965e85223f7af17e58148afc5c51979cb9
Prototype99th <167306705+Prototype99th@users.noreply.github.com>
2024-04-17 20:52:36 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,50 +1,42 @@
 import java.util.Scanner;
 
 public class Main{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Bem vindo à calculadora simples!");
-    System.out.println("Por favor, insira dois números e a operação desejada (+, -, *, /):");
-// Receptor de números e a operação do usuário
-    System.out.print("Número 1: ");
-    int num1= sc.nextInt();
-    System.out.print("Número 2: ");
-    int num2= sc.nextInt();
-    System.out.print("Operação (+, -, *, /): ");
-    char operador = sc.next().charAt(0);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-// Verificando a operação e realizando o cálculo correspondente
-    if(operador == '+'){
-      System.out.println(soma (num1,num2));
-    }else if(operador == '-'){
-        System.out.println(subtracao (num1,num2));
-      }
-    else if(operador == '*'){
-      System.out.println(multiplicacao (num1,num2));
-    }
-    else if(operador == '/'){
-      if (num2 != 0){
-      System.out.println("O resultado é: " + divisao(num1,num2));
-    }else{
-        System.out.println("Erro: Divisão por zero!");
-          return;
+    System.out.print("Digite uma frase: ");
+    String inputString = scanner.nextLine();
+
+    StringProcessor processor = new StringProcessor(inputString);
+
+    System.out.println("Frase em maiúsculo: " + processor.toUpperCase());
+    System.out.println("Frase em minúsculo: " + processor.toLowerCase());
+    System.out.println("Número de caracteres: " + processor.length());
+    System.out.println("Frase invertida: " + processor.reverse());
+
+    scanner.close();
   }
-    }else{
-      System.out.println("Operação invalida");
-    }
+}
+
+class StringProcessor {
+  private String inputString;
+
+  public StringProcessor(String inputString) {
+    this.inputString = inputString;
   }
 
-  // Métodos para o cálculo das operações
-  public static int soma(int a, int b){
-    return a+b;
+  public String toUpperCase() {
+    return inputString.toUpperCase();
   }
-  public static int multiplicacao(int a, int b){
-    return a*b;
+
+  public String toLowerCase() {
+    return inputString.toLowerCase();
   }
-  public static int divisao(int a, int b){
-    return a/b;
+
+  public int length() {
+    return inputString.length();
   }
-  public static int subtracao(int a, int b){
-    return a-b;
+  public String reverse() {
+    return new StringBuilder(inputString).reverse().toString();
   }
 }
